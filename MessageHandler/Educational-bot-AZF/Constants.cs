d89e27a7b84a80e77fef522e05e28f457f534f85
Nicolas Civade <mopigno@gmail.com>
2022-06-29 09:26:54 +0200

    initial push

---
+++ MessageHandler/Educational-bot-AZF/Constants.cs
@@ -0,0 +1,43 @@
+﻿// <copyright file="Constants.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Educational_bot_AZF
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Constant class.
+    /// </summary>
+    public class Constants
+    {
+        /// <summary>
+        /// App registration Id.
+        /// </summary>
+        public const string AppId = "0093bf30-36bf-44e1-bb8c-77cd5867d67b";
+
+        /// <summary>
+        /// Tenant Id.
+        /// </summary>
+        public const string TenantId = "fefe9af7-f330-429d-8087-f5e656f7a7ce";
+
+        /// <summary>
+        /// App registration secret.
+        /// </summary>
+        public const string ClientSecret = "Lbr8Q~MGDrqclhP~bSPTEbtfcorbRFqEcjQKdcn8";
+
+        /// <summary>
+        /// Graph token scheme.
+        /// </summary>
+        public const string TokenScheme = "Bearer";
+
+        /// <summary>
+        /// Key of configuration corresponding to the authorized scopes of the application using Graph.
+        /// </summary>
+        public static readonly string[] Scopes = { "User.Read" };
+    }
+}
---
+++ MessageHandler/Educational-bot-AZF/MessageHandler.cs
@@ -0,0 +1,51 @@
+
+namespace Educational_bot_AZF
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Threading.Tasks;
+    using Azure.Core;
+    using Azure.Identity;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Azure.WebJobs;
+    using Microsoft.Azure.WebJobs.Extensions.CosmosDB;
+    using Microsoft.Azure.WebJobs.Extensions.Http;
+    using Microsoft.Extensions.Logging;
+    using Microsoft.Graph;
+    using Microsoft.Identity.Client;
+    using Newtonsoft.Json;
+
+    public static class MessageHandler
+    {
+
+        [FunctionName("MessageHandler")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            var tenantId = Constants.TenantId;
+            var clientId = Constants.AppId;
+            var clientSecret = Constants.ClientSecret;
+
+            string[] scopes = { "https://graph.microsoft.com/.default" };
+
+            ClientSecretCredential clientSecretCredential = new ClientSecretCredential(tenantId, clientId, clientSecret);
+
+            GraphServiceClient graphClient = new GraphServiceClient(clientSecretCredential, scopes);
+
+
+            string messageId = "1655207535262";
+            string channelId = "19:rC3Cn3ch-duulIX1tFnkDGYOse9NNzl45jpPXmPH6wU1@thread.tacv2";
+            string teamId = "1f9e5c34-74c5-48af-ac17-1989f454729f";
+
+
+            var answers = await graphClient.Teams[teamId].Channels.Request().GetAsync();
+            //var messages = await graphClient.Teams[teamId].Channels[channelId].Messages[messageId].Replies.Request().GetAsync();
+
+
+            return new OkObjectResult(answers);
+        }
+    }
+}
